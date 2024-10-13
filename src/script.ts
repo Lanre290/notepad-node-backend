dd2014b161386e74b9857c205733a365e66ce2f2
Ashiru Olanrewaju <lanre2967@gmail.com>
2024-10-13 20:04:25 +0100

    breakpoint

--- src/script.ts
+++ src/script.ts
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 import dotenv from "dotenv";
-import { NextFunction, Request } from "express";
+import { NextFunction, Request, Response } from "express";
 import { Pool } from "pg";
 import bcrypt from "bcryptjs";
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const session = require('express-session');
 
 dotenv.config();
 
@@ -33,6 +35,17 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: SQL_PORT,
 });
+
+//middlewares
+app.use(cors(corsOption));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true}
+}));
 
 const checkTableExists = async (tableName: string) => {
   try {
@@ -114,6 +127,48 @@ const verifyPassword = async (
   }
 };
 
+const sendOTP = async (userId: number, email: string, name: string, otp:number) => {
+  try {
+
+    const mailOptions = {
+      from: "your-email@example.com",
+      to: email,
+      subject: "Verify your Identity",
+      html: `Hello ${
+        name.split(" ").length > 1 ? name.split(" ")[1] : name.split(" ")[0]
+      }, Confirm your email.
+                    OTP CODE
+                    ${otp}
+                    Please enter the following OTP to complete your verification process, if you did not make this request, you can safely ignore this email.
+
+
+
+                    Your friendly neighborhood TechBro - BroCode. 💝 
+                    `,
+    };
+
+    transporter.sendMail(mailOptions, async (error: any, info: any) => {
+      if (error) {
+        console.log('error');
+      } else {
+        return true
+      }
+    });
+  } catch (error) {
+    return "Error sending mail";
+  }
+};
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: true,
+  auth: {
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD,
+  },
+});
+
 const authenticateToken = (
   req: Request | any,
   res: any,
@@ -122,7 +177,7 @@ const authenticateToken = (
   const token = (req.headers.authorization as string).split(" ")[1];
 
   if (!token) {
-    res.status(401).json({ sucess: false, error: "Pleaseproide a token." });
+    res.status(401).json({ sucess: false, error: "Please provide a token." });
   }
 
   jwt.verify(token, secretKey, (err: any, decoded: any) => {
@@ -139,62 +194,69 @@ const authenticateToken = (
 ensureTableExists("users");
 ensureTableExists("notes");
 
-//middlewares
-app.use(cors(corsOption));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.post(
+  "/api/auth/signup",
+  async (req: Request | any, res: any) => {
+    const { name, email, pwd } = req.body;
 
-app.post("/api/auth/signup", async (req: Request, res: any) => {
-  const { name, email, pwd } = req.body;
+    console.log(req.body);
 
-  console.log(req.body);
+    if (!name || !email || !pwd) {
+      res.status(401).json({ error: "Unexpected request." });
+    } else {
+      try {
+        const emailExistsQuery: any = await pool.query(
+          "SELECT COUNT(*) FROM users WHERE email=$1",
+          [email]
+        );
 
-  if (!name || !email || !pwd) {
-    res.status(401).json({ error: "Unexpected request." });
-  } else {
-    try {
-      const emailExistsQuery: any = await pool.query(
-        "SELECT COUNT(*) FROM users WHERE email=$1",
-        [email]
-      );
+        let emailExistsNo = emailExistsQuery.rows[0].count;
 
-      let emailExistsNo = emailExistsQuery.rows[0].count;
+        if (emailExistsNo > 0) {
+          res.status(409).json({ error: "Email already exists." });
+        } else {
+          const query = `INSERT INTO users (name, email, pwd) VALUES ($1, $2, $3) RETURNING id,email,name`;
 
-      if (emailExistsNo > 0) {
-        res.status(409).json({ error: "Email already exists." });
-      } else {
-        const query = `INSERT INTO users (name, email, pwd) VALUES ($1, $2, $3) RETURNING id,email,name`;
+          let encrytedPassword = await encryptPassword(pwd);
 
-        let encrytedPassword = await encryptPassword(pwd);
+          let queryResult = await pool.query(query, [
+            name,
+            email,
+            encrytedPassword,
+          ]);
+          const userId: number = queryResult.rows[0].id;
+          const userEmail: string = queryResult.rows[0].email;
+          const username: string = queryResult.rows[0].name;
 
-        let queryResult = await pool.query(query, [
-          name,
-          email,
-          encrytedPassword,
-        ]);
-        const userId: number = queryResult.rows[0].id;
-        const userEmail: string = queryResult.rows[0].email;
-        const username: string = queryResult.rows[0].name;
+          const token = jwt.sign({ id: userId }, secretKey, {
+            expiresIn: "3h",
+          });
 
-        const token = jwt.sign({ id: userId }, secretKey, { expiresIn: "3h" });
+          const user = {
+            id: userId,
+            email: userEmail,
+            name: username,
+          };
 
-        const user = {
-          id: userId,
-          email: userEmail,
-          name: username,
-        };
-
-        res.status(200).json({ success: true, data: user, token: token });
+          res.status(200).json({ success: true, data: user, token: token });
+          const otp = Math.floor(1000 + Math.random() * 9000);
+          sendOTP(userId, userEmail, username, otp);
+          req.session.otp = otp;
+          req.session.user = user;
+          req.id = userId;
+          req.email = userEmail;
+          req.name = username;
+        }
+      } catch (error) {
+        res
+          .status(500)
+          .json({ error: "Internal server error", errorData: error });
       }
-    } catch (error) {
-      res
-        .status(500)
-        .json({ error: "Internal server error", errorData: error });
     }
   }
-});
+);
 
-app.post("/api/auth/login", async (req: Request, res: any) => {
+app.post("/api/auth/login", async (req: Request, res: Response) => {
   try {
     const { email, pwd } = req.body;
 
@@ -224,7 +286,7 @@ app.post("/api/auth/login", async (req: Request, res: any) => {
         try {
           const match = await bcrypt.compare(pwd, user.pwd);
           if (match) {
-            res.redirect("/home");
+            res.status(200).json({success: true});
           } else {
             res
               .status(409)
@@ -241,6 +303,31 @@ app.post("/api/auth/login", async (req: Request, res: any) => {
     }
   } catch (error) {}
 });
+
+app.post('/otp', function (req:Request | any, res:any) {
+  const userId = req.user.id;
+  const userEmail = req.user.email;
+  const username = req.user.name;
+
+  if(!userId || !userEmail || !username){
+    res.status(401).json({'error': 'Unauthorized acess.'});
+  }
+  const otp = Math.floor(1000 + Math.random() * 9000);
+  sendOTP(userId, userEmail, req.user.name, otp);
+  req.session.otp = otp;
+});
+
+app.post('/verify-otp', function (req:Request | any, res:any) {
+  let {otp} = req.body;
+
+  if(otp != req.session.otp){
+    res.status(409).json({'error': 'Incorrect otp entered.'});
+  }
+  else{
+    res.status(200).json({success: true});
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`server is listening on port: ${PORT}`);
